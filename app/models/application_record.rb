9b5b0f8cfbbc790a70fe19cc7fb0670e22556d01
Nee Kin <njgzs@126.com>
2026-07-04 03:17:43 +0800

    fix: 填充 versions.metadata 列 (user_agent, user_id)

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -7,6 +7,9 @@ class ApplicationRecord < ActiveRecord::Base
       meta: {
         request_id: ->(_record) { Current.request_id },
         ip: ->(_record) { Current.ip },
+        metadata: ->(_record) {
+          { user_agent: Current.user_agent, user_id: Current.user&.id }.compact.to_json
+        },
       }
     )
   end

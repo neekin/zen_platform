735a57a95d72ed5b57d763304b376cea7830c6a9
Nee Kin <njgzs@126.com>
2026-07-05 05:20:21 +0800

    chore: 删除 Article 测试示例代码
    - rails destroy zen:admin Article
    - 清理侧边栏菜单配置

--- app/policies/article_policy.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class ArticlePolicy < ApplicationPolicy
-  def index? = check_permission(:index)
-  def show? = check_permission(:show)
-  def create? = check_permission(:create)
-  def update? = check_permission(:update)
-  def destroy? = check_permission(:destroy)
-end
